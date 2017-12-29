4c6d624b18d7ec99144b9b939d696e833ed5781f
slamice <issam@rocketrip.com>
2017-12-28 23:05:13 -0500

    added db url setting

--- backend/settings.py
+++ backend/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.environ.get('INVESTED_DATABASE_NAME'),
         'USER': os.environ.get('INVESTED_DATABASE_USERNAME'),
         'PASSWORD': os.environ.get('INVESTED_PASSWORD'),
-        'HOST': '127.0.0.1',
+        'HOST': os.environ.get('INVESTED_DATABASE_URL'),
         'PORT': '5432',
     }
 }
